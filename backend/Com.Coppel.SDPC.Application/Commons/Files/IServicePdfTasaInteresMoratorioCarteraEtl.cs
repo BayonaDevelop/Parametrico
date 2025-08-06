b41e163f641f9d24b18d5a77be1d729142846c7c
Fernando Bayona Garcia <nem.fbayona@coppel.com>
2025-08-05 19:12:46 -0700

    Interfaces

---
+++ backend/Com.Coppel.SDPC.Application/Commons/Files/IServicePdfTasaInteresMoratorioCarteraEtl.cs
@@ -0,0 +1,8 @@
+﻿using Com.Coppel.SDPC.Application.Models.Reports;
+
+namespace Com.Coppel.SDPC.Application.Commons.Files;
+
+public interface IServicePdfTasaInteresMoratorioCarteraEtl
+{
+	string CreateFile(FileContentVM request, string logoPath);
+}
